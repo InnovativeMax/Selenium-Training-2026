6d232c1f8359743608f6a7d90ef535019d570a2f
Dheeraj V <128276207+dheevj247@users.noreply.github.com>
2026-08-20 10:41:32 +0530

    Packages for suma & Ragini

---
+++ src/test/java/com/selenium/task/ragini/Demo.java
@@ -0,0 +1,7 @@
+package com.selenium.task.ragini;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/selenium/task/suma/Demo.java
@@ -0,0 +1,7 @@
+package com.selenium.task.suma;
+
+public class Demo {
+    public static void main(String[] args) {
+        
+    }
+}
