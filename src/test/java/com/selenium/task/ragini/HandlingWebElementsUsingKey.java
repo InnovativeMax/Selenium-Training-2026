6557262d556330b097268ea09dd30f18b4001ec3
Raginideekshit <raginim678@gmail.com>
2026-09-16 11:40:49 +0530

    New Code

---
+++ src/test/java/com/selenium/task/ragini/HandlingWebElementsUsingKey.java
@@ -0,0 +1,30 @@
+package com.selenium.task.ragini;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingWebElementsUsingKey {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://www.saucedemo.com/";
+        String username = "standard_user";
+        String password = "secret_sauce";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        System.out.println("Entered username : "+username);
+        Thread.sleep(4000);
+        driver.findElement(By.id("password")).sendKeys(password);
+        System.out.println("Entered password : "+password);
+        Thread.sleep(4000);
+        driver.findElement(By.id("login-button")).sendKeys(Keys.ENTER);
+        System.out.println("Clicked on login");
+        Thread.sleep(2000);
+        driver.quit();
+
+}
+    }
